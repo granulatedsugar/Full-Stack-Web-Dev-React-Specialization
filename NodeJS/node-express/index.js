fe4e9619f5492bbb6597e4b2928e983ce1df20bb
niro <roquenikko@gmail.com>
2021-03-25 10:10:23 -0700

    Express serve static files

--- NodeJS/node-express/index.js
+++ NodeJS/node-express/index.js
@@ -1,22 +1,31 @@
+// Import modules
 const express = require('express');
 const http = require('http');
+const morgan = require('morgan');
 
+// Port  and Hostname declaration
 const hostname = 'localhost';
 const port = 3000;
 
 
 const app = express();
+// Will print additional information for development
+app.use(morgan('dev'));
+
+// Serve html files from public folders
+app.use(express.static(__dirname + '/public'))
 
 app.use((req, res, next) => {
-    console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an express server</h1></body></html>');
 });
 
 
+// Create server
 const server = http.createServer(app);
 
+// Start server
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}`)
 });
